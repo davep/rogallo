52daa7c8eaec3cc31b1d6058308c86fec4ca7973
Dave Pearson <davep@davep.org>
2026-06-19 19:33:53 +0100

    :sparkles: Add a function for checking for capsule links

---
+++ src/rogallo/app/preflight.py
@@ -0,0 +1,34 @@
+"""Functions for testing locations."""
+
+##############################################################################
+# Python imports.
+from urllib.parse import urlparse
+
+##############################################################################
+# Wasat imports.
+from wasat import GeminiURI, URIError
+
+
+##############################################################################
+def is_likely_capsule(uri: str) -> bool:
+    """Determine if a URI is likely a capsule.
+
+    Args:
+        uri: The URI to check.
+
+    Returns:
+        `True` if the URI is likely a capsule, `False` otherwise.
+    """
+
+    # Check if it's a straight-up Gemini URI.
+    try:
+        _ = GeminiURI(uri)
+        return True
+    except URIError:
+        pass
+
+    # If it has a scheme at this point, it's not a capsule.
+    return not urlparse(uri).scheme
+
+
+### location_tests.py ends here
