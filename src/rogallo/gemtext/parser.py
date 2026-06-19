a394de959f0e81af929c508c22438289f22389f9
Dave Pearson <davep@davep.org>
2026-06-19 09:05:21 +0100

    :hammer: A link most have a space after the arrow

--- src/rogallo/gemtext/parser.py
+++ src/rogallo/gemtext/parser.py
@@ -129,7 +129,7 @@ class Gemtext:
         pre_formatted = False
         for line in self.text.splitlines():
             if line.startswith("=>"):
-                uri, _, description = line.removeprefix("=>").strip().partition(" ")
+                uri, _, description = line.removeprefix("=> ").strip().partition(" ")
                 yield Link(uri, description)
             elif line.startswith("> "):
                 _, _, quote_text = line.partition(" ")
