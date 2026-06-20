7f71c1228fca3b4c79d9263357e121ed5f80b1cb
Dave Pearson <davep@davep.org>
2026-06-20 09:43:45 +0100

    :sparkles: Add a function for checking for doc-relative URIs

--- src/rogallo/app/preflight.py
+++ src/rogallo/app/preflight.py
@@ -10,6 +10,19 @@ from wasat import GeminiURI, URIError
 
 
 ##############################################################################
+def is_likely_page_relative(uri: str) -> bool:
+    """Determine if a URI is likely a relative URI.
+
+    Args:
+        uri: The URI to check.
+
+    Returns:
+        `True` if the URI is likely a relative URI, `False` otherwise.
+    """
+    return not urlparse(uri).scheme
+
+
+##############################################################################
 def is_likely_capsule(uri: str) -> bool:
     """Determine if a URI is likely a capsule.
 
@@ -19,14 +32,13 @@ def is_likely_capsule(uri: str) -> bool:
     Returns:
         `True` if the URI is likely a capsule, `False` otherwise.
     """
-
-    # Check if it's a straight-up Gemini URI.
     try:
+        # Check if it's a straight-up Gemini URI.
         _ = GeminiURI(uri)
         return True
     except URIError:
-        # If it has a scheme at this point, it's not a capsule.
-        return not urlparse(uri).scheme
+        # If it's not, check if it's likely a relative URI.
+        return is_likely_page_relative(uri)
 
 
 ### location_tests.py ends here
--- tests/unit/test_preflight.py
+++ tests/unit/test_preflight.py
@@ -6,7 +6,25 @@ from pytest import mark
 
 ##############################################################################
 # Local imports.
-from rogallo.app.preflight import is_likely_capsule
+from rogallo.app.preflight import is_likely_capsule, is_likely_page_relative
+
+
+##############################################################################
+@mark.parametrize(
+    "uri, result",
+    [
+        ("/", True),
+        ("relative/path", True),
+        ("http://example.com", False),
+        ("gemini://example.com", False),
+        ("ftp://example.com", False),
+        ("", True),
+        ("   ", True),
+    ],
+)
+def test_is_likely_page_relative(uri: str, result: bool) -> None:
+    """Test the is_likely_page_relative function."""
+    assert is_likely_page_relative(uri) is result
 
 
 ##############################################################################
