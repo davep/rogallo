68bb82c6762279231b09be7d84910c390cf44512
Dave Pearson <davep@davep.org>
2026-06-25 20:34:22 +0100

    :white_check_mark: Add some tests for preflight.path_from_uri

--- tests/unit/test_preflight.py
+++ tests/unit/test_preflight.py
@@ -1,12 +1,16 @@
 """Unit tests for the Preflight module."""
 
 ##############################################################################
+# Python imports.
+from pathlib import Path
+
+##############################################################################
 # Pytest imports.
-from pytest import mark
+from pytest import mark, raises
 
 ##############################################################################
 # Local imports.
-from rogallo.preflight import is_likely_capsule, is_likely_page_relative
+from rogallo.preflight import is_likely_capsule, is_likely_page_relative, path_from_uri
 
 
 ##############################################################################
@@ -45,6 +49,35 @@ def test_is_likely_page_relative(uri: str, result: bool) -> None:
 def test_is_likely_capsule(uri: str, result: bool) -> None:
     """Test the is_likely_capsule function."""
     assert is_likely_capsule(uri) is result
+
+
+##############################################################################
+@mark.parametrize(
+    "uri, result",
+    [
+        ("file:///tmp/test.gmi", Path("/tmp/test.gmi").resolve()),
+        ("/tmp/test.gmi", Path("/tmp/test.gmi").resolve()),
+    ],
+)
+def test_path_from_uri_file(uri: str, result: Path) -> None:
+    """Test the path_from_uri function with a file URI."""
+    assert path_from_uri(uri) == result
+
+
+##############################################################################
+@mark.parametrize(
+    "uri",
+    [
+        ("http://example.com"),
+        ("gemini://example.com"),
+        ("ftp://example.com"),
+        ("//example.com"),
+    ],
+)
+def test_path_from_uri_invalid(uri: str) -> None:
+    """Test the path_from_uri function with an invalid URI."""
+    with raises(ValueError):
+        _ = path_from_uri(uri)
 
 
 ### test_preflight.py ends here
