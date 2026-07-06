193b16f2db98238f9409577c4a3e867c59ca2e9b
Dave Pearson <davep@davep.org>
2026-07-06 20:52:04 +0200

    :hammer: Change where Iterator is imported from

--- src/rogallo/widgets/viewer/widget.py
+++ src/rogallo/widgets/viewer/widget.py
@@ -2,7 +2,8 @@
 
 ##############################################################################
 # Python imports.
-from typing import Iterator, NamedTuple
+from collections.abc import Iterator
+from typing import NamedTuple
 
 ##############################################################################
 # Gemtext imports.
