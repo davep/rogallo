0d77a8ec97dec71c0834c33fa6c882e116377f15
Dave Pearson <davep@davep.org>
2026-07-13 08:04:30 +0100

    :books: Ensure the same starting configuration for each screenshot

--- docs/screenshots/main_screenshot.py
+++ docs/screenshots/main_screenshot.py
@@ -2,12 +2,21 @@ import os
 from argparse import Namespace
 from pathlib import Path
 
+from rogallo.data import update_configuration
 from rogallo.rogallo import Rogallo
 
 docs_dir = Path(__file__).parent.parent
 
 os.environ["XDG_CONFIG_HOME"] = str(docs_dir / "build" / "config")
 os.environ["XDG_DATA_HOME"] = str(docs_dir / "build" / "data")
+
+with update_configuration() as config:
+    config.bookmarks_visble = False
+    config.command_line_on_top = False
+    config.disable_animations = True
+    config.history_visible = False
+    config.home_page = str(docs_dir / "examples/features.gmi")
+    config.with_cache = False
 
 app = Rogallo(
     Namespace(
