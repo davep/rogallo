ba7e3f12685586da65638ea3c7080659e15ca096
Dave Pearson <davep@davep.org>
2026-07-13 09:25:56 +0100

    :books: Explain the content of the screenshot app

--- docs/screenshots/main_screenshot.py
+++ docs/screenshots/main_screenshot.py
@@ -1,3 +1,5 @@
+"""This script is used to generate screenshots of Rogallo for the documentation."""
+
 import os
 from argparse import Namespace
 from pathlib import Path
@@ -7,19 +9,30 @@ from wasat import GeminiURI
 from rogallo.data import Bookmark, save_bookmarks, update_configuration
 from rogallo.rogallo import Rogallo
 
+##############################################################################
+# Work our the root of the documentation directory and the build directory.
 docs_dir = Path(__file__).parent.parent
+docs_build_dir = docs_dir / "build"
 
-os.environ["XDG_CONFIG_HOME"] = str(docs_dir / "build" / "config")
-os.environ["XDG_DATA_HOME"] = str(docs_dir / "build" / "data")
+##############################################################################
+# Set the XDG_ to point at an isolated build environment for the
+# screenshots. I don't want to mess with an actual installation of Rogallo.
+os.environ["XDG_CONFIG_HOME"] = str(docs_build_dir / "config")
+os.environ["XDG_DATA_HOME"] = str(docs_build_dir / "data")
 
+##############################################################################
+# Start out with the same configuration for every screenshot.
 with update_configuration() as config:
     config.bookmarks_visble = False
     config.command_line_on_top = False
     config.disable_animations = True
     config.history_visible = False
     config.home_page = str(docs_dir / "examples/features.gmi")
+    config.theme = "textual-mono"
     config.with_cache = False
 
+##############################################################################
+# Create some bookmarks for the screenshots.
 save_bookmarks(
     [
         Bookmark("The Gemini Protocol", GeminiURI("gemini://geminiprotocol.net/")),
@@ -29,13 +42,19 @@ save_bookmarks(
     ]
 )
 
+##############################################################################
+# Create the Rogallo app with the specified command line arguments. Also
+# create the instance as `app` in the global namespace so that it can be
+# found by and used by the screenshot facility.
 app = Rogallo(
     Namespace(
         command="open",
         location=str(docs_dir / "examples/features.gmi"),
-        theme="textual-mono",
     )
 )
 
+##############################################################################
 if __name__ == "__main__":
     app.run()
+
+### main_screenshot.py ends here
