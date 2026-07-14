e6505d1d25d8f00f311b32f7b45111949414e5af
Dave Pearson <davep@davep.org>
2026-07-14 18:26:09 +0100

    :hammer: Focus and scroll in one go

--- src/rogallo/widgets/viewer/widget.py
+++ src/rogallo/widgets/viewer/widget.py
@@ -143,8 +143,7 @@ class Viewer(Vertical, can_focus=False):
         if self.with_link_numbers and self._jump is not None:
             for link in self._view.query(GemtextLink):
                 if link.jump_number == self._jump:
-                    self._view.scroll_to_widget(link)
-                    link.focus()
+                    link.focus(scroll_visible=True)
                     return
             self._jump = self._jump % 10 if self._jump > 9 else None
 
