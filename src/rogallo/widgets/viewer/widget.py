24b9506bfe2ccbd072c3428e9a1f8498137c3658
Dave Pearson <davep@davep.org>
2026-07-17 16:38:24 +0100

    :hammer: Lean into default classes (more)

--- src/rogallo/widgets/viewer/widget.py
+++ src/rogallo/widgets/viewer/widget.py
@@ -103,7 +103,7 @@ class Viewer(Vertical, can_focus=False):
     def compose(self) -> ComposeResult:
         """Compose the viewer widget."""
         yield ViewerTitle()
-        document = DocumentView(classes="panel")
+        document = DocumentView()
         if (max_width := load_configuration().maximum_document_width) > 0:
             document.styles.max_width = max_width
             yield HorizontalGroup(document, id="document-wrapper", classes="dead-space")
