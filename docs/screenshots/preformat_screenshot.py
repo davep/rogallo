7b77833da030c9c7ecb6aa23ce440412656cacf6
Dave Pearson <davep@davep.org>
2026-07-19 15:20:39 +0100

    :books: Tidy up the documentation screenshot code

--- docs/screenshots/preformat_screenshot.py
+++ docs/screenshots/preformat_screenshot.py
@@ -1,6 +1,10 @@
+"""Generate screenshots related to pre-formatted text."""
+
 from support.maker import make_app
 
 app = make_app("preformat")
 
 if __name__ == "__main__":
     app.run()
+
+### preformat_screenshot.py ends here
