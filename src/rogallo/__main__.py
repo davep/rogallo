9989a4e2bfa60541ef7c30a09df9623598580f52
Dave Pearson <davep@davep.org>
2026-07-26 11:55:10 +0100

    :sparkles: Add port70's version to the diagnostics

--- src/rogallo/__main__.py
+++ src/rogallo/__main__.py
@@ -138,6 +138,7 @@ def show_dignoastics() -> None:
     from bagofstuff import __version__ as bagofstuff_version
     from cryptography import __version__ as cryptography_version
     from gemtext import __version__ as gemtext_version
+    from port70 import __version__ as port70_version
     from port79 import __version__ as port79_version
     from textual import __version__ as textual_version
     from textual_enhanced import __version__ as textual_enhanced_version
@@ -148,6 +149,7 @@ def show_dignoastics() -> None:
     print(f"bagofstuff: {bagofstuff_version}")
     print(f"cryptography: {cryptography_version}")
     print(f"gemtext: {gemtext_version}")
+    print(f"port70: {port70_version}")
     print(f"port79: {port79_version}")
     print(f"rogallo: {__version__}")
     print(f"textual: {textual_version}")
