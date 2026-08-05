2a5ced63229b5e452a3da03852a68ad6e11c7899
Dave Pearson <davep@davep.org>
2026-08-05 10:07:10 +0100

    :books: Simplify how the example themes for the docs are made

--- docs/screenshots/support/maker.py
+++ docs/screenshots/support/maker.py
@@ -4,7 +4,6 @@ import os
 from argparse import Namespace
 from dataclasses import fields
 from datetime import datetime, timedelta
-from json import dumps
 from pathlib import Path
 from random import randint
 from typing import Any
@@ -52,159 +51,14 @@ save_bookmarks(
 ##############################################################################
 # Create some sample custom themes.
 themes_dir().mkdir(parents=True, exist_ok=True)
-(themes_dir() / "white.json").write_text(
-    dumps(
-        {
-            "name": "terminal-white",
-            "primary": "#D8D8D8",
-            "secondary": "#9E9E9E",
-            "accent": "#C4C4C4",
-            "foreground": "#B8B8B8",
-            "background": "#000000",
-            "surface": "#121212",
-            "panel": "#1E1E1E",
-            "success": "#D8D8D8",
-            "warning": "#C4C4C4",
-            "error": "#4A4A4A",
-            "dark": True,
-            "luminosity_spread": 0.15,
-            "text_alpha": 0.95,
-            "variables": {
-                "block-cursor-background": "#D8D8D8",
-                "block-cursor-foreground": "#000000",
-                "block-cursor-text-style": "none",
-                "border": "#D8D8D8",
-                "border-blurred": "#404040",
-                "button-color-foreground": "#000000",
-                "button-focus-text-style": "reverse",
-                "footer-background": "#121212",
-                "footer-description-foreground": "#B8B8B8",
-                "footer-key-foreground": "#D8D8D8",
-                "foreground": "#B8B8B8",
-                "foreground-muted": "#404040",
-                "input-cursor-background": "#C4C4C4",
-                "input-cursor-foreground": "#000000",
-                "input-selection-background": "#9E9E9E 40%",
-                "input-selection-foreground": "#000000",
-                "screen-selection-background": "#9E9E9E 40%",
-                "screen-selection-foreground": "#B8B8B8",
-                "scrollbar": "#9E9E9E",
-                "scrollbar-active": "#C4C4C4",
-                "scrollbar-background": "#000000",
-                "scrollbar-background-active": "#1E1E1E",
-                "scrollbar-background-hover": "#121212",
-                "scrollbar-corner-color": "#000000",
-                "scrollbar-hover": "#D8D8D8",
-                "text": "#B8B8B8",
-                "text-disabled": "#404040",
-                "text-muted": "#404040",
-            },
-        },
-        indent=4,
+for theme in ("white", "amber", "green"):
+    (themes_dir() / Path(theme).with_suffix(".json")).write_text(
+        (
+            Path(__file__).parent.parent.parent.parent
+            / "example-themes"
+            / Path(theme).with_suffix(".json")
+        ).read_text()
     )
-)
-(themes_dir() / "amber.json").write_text(
-    dumps(
-        {
-            "name": "terminal-amber",
-            "primary": "#FFB000",
-            "secondary": "#C88200",
-            "accent": "#FFC947",
-            "foreground": "#FFC107",
-            "background": "#000000",
-            "surface": "#140E00",
-            "panel": "#241800",
-            "success": "#FFB000",
-            "warning": "#FFDC00",
-            "error": "#593800",
-            "dark": True,
-            "luminosity_spread": 0.15,
-            "text_alpha": 0.95,
-            "variables": {
-                "block-cursor-background": "#FFB000",
-                "block-cursor-foreground": "#000000",
-                "block-cursor-text-style": "none",
-                "border": "#FFB000",
-                "border-blurred": "#4D3300",
-                "button-color-foreground": "#000000",
-                "button-focus-text-style": "reverse",
-                "footer-background": "#140E00",
-                "footer-description-foreground": "#FFC107",
-                "footer-key-foreground": "#FFB000",
-                "foreground": "#FFC107",
-                "foreground-muted": "#4D3300",
-                "input-cursor-background": "#FFC947",
-                "input-cursor-foreground": "#000000",
-                "input-selection-background": "#C88200 40%",
-                "input-selection-foreground": "#000000",
-                "screen-selection-background": "#C88200 40%",
-                "screen-selection-foreground": "#FFC107",
-                "scrollbar": "#C88200",
-                "scrollbar-active": "#FFC947",
-                "scrollbar-background": "#000000",
-                "scrollbar-background-active": "#241800",
-                "scrollbar-background-hover": "#140E00",
-                "scrollbar-corner-color": "#000000",
-                "scrollbar-hover": "#FFB000",
-                "text": "#FFC107",
-                "text-disabled": "#4D3300",
-                "text-muted": "#4D3300",
-            },
-        },
-        indent=4,
-    )
-)
-(themes_dir() / "green.json").write_text(
-    dumps(
-        {
-            "name": "terminal-green",
-            "primary": "#33FF33",
-            "secondary": "#00AA44",
-            "accent": "#66FF66",
-            "foreground": "#33FF33",
-            "background": "#000000",
-            "surface": "#001A08",
-            "panel": "#003311",
-            "success": "#33FF33",
-            "warning": "#CCFF33",
-            "error": "#004D1A",
-            "dark": True,
-            "luminosity_spread": 0.15,
-            "text_alpha": 0.95,
-            "variables": {
-                "block-cursor-background": "#33FF33",
-                "block-cursor-foreground": "#000000",
-                "block-cursor-text-style": "none",
-                "border": "#33FF33",
-                "border-blurred": "#004D1A",
-                "button-color-foreground": "#000000",
-                "button-focus-text-style": "reverse",
-                "footer-background": "#001A08",
-                "footer-description-foreground": "#33FF33",
-                "footer-key-foreground": "#33FF33",
-                "foreground": "#33FF33",
-                "foreground-muted": "#004D1A",
-                "input-cursor-background": "#66FF66",
-                "input-cursor-foreground": "#000000",
-                "input-selection-background": "#00AA44 40%",
-                "input-selection-foreground": "#000000",
-                "screen-selection-background": "#00AA44 40%",
-                "screen-selection-foreground": "#33FF33",
-                "scrollbar": "#00AA44",
-                "scrollbar-active": "#66FF66",
-                "scrollbar-background": "#000000",
-                "scrollbar-background-active": "#003311",
-                "scrollbar-background-hover": "#001A08",
-                "scrollbar-corner-color": "#000000",
-                "scrollbar-hover": "#33FF33",
-                "text": "#33FF33",
-                "text-disabled": "#004D1A",
-                "text-muted": "#004D1A",
-            },
-        },
-        indent=4,
-    )
-)
 
 
 ##############################################################################
