d3b5c33302156609a093502b188b306aa5b5a5b0
Dave Pearson <davep@davep.org>
2026-08-10 14:50:07 +0100

    :rewind: Revert DRYing of loader dispatch
    While pyright can handling the typing, seems mypy can't.

--- src/rogallo/screens/main/screen.py
+++ src/rogallo/screens/main/screen.py
@@ -591,12 +591,16 @@ class Main(EnhancedScreen[None]):
         Args:
             message: The message the location open request.
         """
-        {
-            FingerURI: self._load_from_finger,
-            GeminiURI: self._load_from_gemini,
-            GopherURI: self._load_from_gopher,
-            SpartanURI: self._load_from_spartan,
-        }.get(type(message.location), self._load_from_filesystem)(message)
+        if isinstance(message.location, FingerURI):
+            self._load_from_finger(message)
+        elif isinstance(message.location, GeminiURI):
+            self._load_from_gemini(message)
+        elif isinstance(message.location, GopherURI):
+            self._load_from_gopher(message)
+        elif isinstance(message.location, SpartanURI):
+            self._load_from_spartan(message)
+        else:
+            self._load_from_filesystem(message)
 
     @on(OpenURI)
     def open_uri(self, message: OpenURI) -> None:
