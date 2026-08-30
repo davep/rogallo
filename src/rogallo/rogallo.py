662aa69c40e9c275b6f7bb750fea08764a071926
Dave Pearson <davep@davep.org>
2026-08-30 09:26:51 +0100

    :lipstick: Fix the scrollbar sizes
    Adding CSS to the app nuked the CSS inherited from EnhancedApp.

--- src/rogallo/rogallo.py
+++ src/rogallo/rogallo.py
@@ -60,13 +60,15 @@ class Rogallo(EnhancedApp[None]):
 
     COMMANDS = set()
 
-    CSS = """
-    Tooltip {
+    CSS = f"""
+    {EnhancedApp.CSS}
+
+    Tooltip {{
         padding: 0 2;
         background: $panel 80%;
         border-top: hkey $panel 20%;
         border-bottom: hkey $panel 20%;
-    }
+    }}
     """
 
     BINDINGS = (
