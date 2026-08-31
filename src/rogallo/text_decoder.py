37ed2ab52eb187322c5a5b646d7fa60a60f6c5e0
Dave Pearson <davep@davep.org>
2026-08-31 08:05:56 +0100

    :hammer: Allow a response's charset to win

--- src/rogallo/text_decoder.py
+++ src/rogallo/text_decoder.py
@@ -12,6 +12,25 @@ from wasat import Response as GeminiResponse
 
 
 ##############################################################################
+def _best_encoding(response: GeminiResponse | SpartanResponse) -> dict[str, str]:
+    """Determine the best encoding to use for decoding the response.
+
+    Args:
+        response: The response to determine the encoding for.
+
+    Returns:
+        The best encoding to use for decoding the response.
+    """
+    if response.charset:
+        return {
+            "encoding": "utf-8-sig"
+            if response.charset.lower() == "utf-8"
+            else response.charset
+        }
+    return {}
+
+
+##############################################################################
 async def decode_text(response: GeminiResponse | SpartanResponse) -> str:
     """Decode the text from a response.
 
@@ -24,7 +43,7 @@ async def decode_text(response: GeminiResponse | SpartanResponse) -> str:
     try:
         # Give the library a chance to decode the text using the charset
         # specified in the response headers.
-        return await response.text(encoding="utf-8-sig")
+        return await response.text(**_best_encoding(response))
     except (GeminiProtocolError, SpartanResponseError):
         # If the library fails to decode the text, fall back to latin-1.
         return await response.text(encoding="latin-1")
