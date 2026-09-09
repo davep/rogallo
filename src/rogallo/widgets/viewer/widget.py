8fca32ec951a6f30dd52244cbe0de5c1260adc56
Dave Pearson <davep@davep.org>
2026-09-09 10:34:33 +0100

    :sparkles: Allow h to move backwards through the links
    This matches l, for going forward. While l was meant to be "link", rather
    than some some of vim-like thing, it makes sense to support people's muscle
    memory given that left and right arrows do the same previous/next link
    movement.

--- src/rogallo/widgets/viewer/widget.py
+++ src/rogallo/widgets/viewer/widget.py
@@ -131,7 +131,7 @@ class Viewer(Vertical, can_focus=False):
 
     BINDINGS = [
         HelpfulBinding(
-            "left, shift+up, L",
+            "left, shift+up, L, h",
             "previous_link",
             tooltip="Move backwards through each of the links",
         ),
