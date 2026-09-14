092e3634a176e1240ff4cc55171e85709f5dcf9f
Dave Pearson <davep@davep.org>
2026-09-14 09:25:16 +0100

    :sparkles: Add a helper function for loading config into a data class

--- src/rogallo/data/configuration/_loader.py
+++ src/rogallo/data/configuration/_loader.py
@@ -1,6 +1,11 @@
 """Provides a utility function for loading configuration data."""
 
 ##############################################################################
+# Python imports.
+from dataclasses import Field, asdict, fields, is_dataclass
+from typing import Any, ClassVar, Protocol
+
+##############################################################################
 # PyYAML imports.
 from yaml import YAMLError, safe_dump, safe_load
 
@@ -28,6 +33,39 @@ def load_configuration[T](name: str, default: T) -> T:
         return safe_load(config_file.read_text(encoding="utf-8")) or default
     except (OSError, YAMLError):
         return default
+
+
+##############################################################################
+class ConfigurationClass(Protocol):
+    """Protocol that helps declare a type as being a dataclass."""
+
+    __dataclass_fields__: ClassVar[dict[str, Field[Any]]]
+
+
+##############################################################################
+def load_configuration_into[T: ConfigurationClass](
+    configuration_class: type[T], name: str
+) -> T:
+    """Load the configuration data from the given file into a dataclass.
+
+    Args:
+        configuration_class: The class to load the configuration info.
+        name: The name of the configuration file.
+
+    Returns:
+        An instance of the class populated with the loaded configuration data.
+    """
+    assert is_dataclass(configuration_class)
+    wanted = {field.name for field in fields(configuration_class) if field.init}
+    return configuration_class(
+        **{
+            key: value
+            for key, value in load_configuration(
+                name, asdict(configuration_class())
+            ).items()
+            if key in wanted
+        }
+    )
 
 
 ### _loader.py ends here
